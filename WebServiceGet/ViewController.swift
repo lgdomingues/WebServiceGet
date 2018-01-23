cb2e2410819863dbf7d4e42b97688b4abbd47791
Calebe <calebenp@gmail.com>
2018-01-22 22:03:20 -0200

    Initial Commit

---
+++ WebServiceGet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServiceGet
+//
+//  Created by Calebe on 22/01/2018.
+//  Copyright © 2018 Calebe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
